90633e2d1234d67919f615e07ba72b917ac75f66
Chris Johnston <cmjohnston@gmail.com>
2019-05-06 15:36:55 -0500

    Challenge 2 in Objects

--- assignments/objects.js
+++ assignments/objects.js
@@ -17,7 +17,6 @@ const example = {
   "gender": "F"
 }
 
-console.log(Mitzi);
 
 // Write your intern objects here:
 const Mitzi = {
@@ -60,15 +59,15 @@ const Antonietta = {
 // Once your objects are created, log out the following requests from HR into the console:
 
 // Mitzi's name
-
+console.log(Mitzi.name);
 // Kennan's ID
-
+console.log(Kennan.id);
 // Keven's email
-
+console.log(Kevan.email);
 // Gannie's name
-
+console.log(Gannie.name);
 // Antonietta's Gender
-
+console.log(Antonietta.gender);
 // ==== Challenge 3: Object Methods ==== 
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
 // console.log(kennan.speak());
