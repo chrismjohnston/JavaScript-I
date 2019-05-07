be5e6b581f1bf479b5aaab55dbf6300ada7bd464
Chris Johnston <cmjohnston@gmail.com>
2019-05-06 19:20:34 -0500

    finished Challenge 4 in Arrays

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -86,7 +86,11 @@ console.log(carModels.sort());
 // ==== Challenge 4 ====
 // The accounting team needs all the years from every car on the lot. Create a new array from the dealer data containing only the car years and log the result in the console.
 let carYears = [];
-console.log();
+let carList = [];
+for (let i = 0; i < inventory.length; i++){
+    carList.push(inventory[i].car_year);
+}
+console.log(carList);
 
 // ==== Challenge 5 ====
 // The car lot manager needs to find out how many cars are older than the year 2000. Using the carYears array you just created, find out how many cars were made before the year 2000 by populating the array oldCars and logging it's length.
@@ -97,6 +101,4 @@ console.log();
 // A buyer is interested in seeing only BMW and Audi cars within the inventory.  Return an array that only contains BMW and Audi cars.  Once you have populated the BMWAndAudi array, use JSON.stringify() to show the results of the array in the console.
 let BMWAndAudi = [];
 console.log();
-
-
 
